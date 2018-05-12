1c51e999214c88f8275d354a04ee1aaf5e654a78
vkapsky <34576244+vkapsky@users.noreply.github.com>
2018-05-12 11:50:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var http 	= require('http');
+var fs 		= require('fs');
+var port 	= "1111" ;
+
+http.createServer(function(request, response) {
+ 
+    response.writeHead(200, {
+        'Content-Type': 'text/json',
+		'Access-Control-Allow-Origin': '*',
+		'X-Powered-By':'nodejs'
+    });
+
+
+    fs.readFile('timetable.json', function(err, content){
+        response.write(content);
+        response.end();
+    });
+
+}).listen(port);
+
+console.log("Listening on port " + port );
